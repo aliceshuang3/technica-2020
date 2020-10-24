a9e5ed978121edcb1a6da274fb52e4d39c5a9904
Jacquelyn Chow <jchow1@swarthmore.edu>
2020-10-24 15:19:51 -0400

    worked on index and added a couple routes

--- init.py
+++ init.py
@@ -22,11 +22,24 @@ print('finished creating 500 business reviews')
 
 # index route
 @app.route("/")
-
 def index():
-
     return render_template("index.html")
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route("/inspiration", methods=["GET"])
+def inspiration():
+    return render_template("inspo.html")
+
+@app.route("/funding", methods=["GET"])
+def funding():
+    return render_template("funding.html")
 
 if __name__ == "__main__":
     app.run()
