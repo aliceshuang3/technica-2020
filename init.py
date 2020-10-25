61167b0dfc391207a953ef0b3cdb9fd1bcaa4813
sojin-lim <genie9038@gmail.com>
2020-10-25 15:09:58 +0900

    working on signup style

--- init.py
+++ init.py
@@ -10,20 +10,6 @@ socketio = SocketIO(app)
 client = MongoClient('mongodb+srv://Sojin:technica@cluster0.e0mte.mongodb.net/<Technica>?ssl=true&ssl_cert_reqs=CERT_NONE')
 db = client.Users
 
-names = ['Alice','Brody','Cindy', 'Genie', 'Jack']
-pw = [0,1,2,3,4]
-
-# for x in range(0, 5):
-#     user = {
-#         'name' : names[x],
-#         'pw' : pw[x]
-#     }
-#
-#     result=db.Users.insert(user)
-#
-# print('finished creating 500 business reviews')
-
-
 # index route
 @app.route("/")
 def index():
